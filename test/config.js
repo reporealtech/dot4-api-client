c9c034202570ba82e6fcb9d6d43bd62c8cecae90
Tobias Ceska <tobias.ceska@realtech.com>
2020-07-17 15:09:38 +0200

    test config

---
+++ test/config.js
@@ -0,0 +1,27 @@
+/**
+ * @copyright Copyright (C) REALTECH AG, Germany - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by Tobias Ceska <tobias.ceska@realtech.com>, December 2019
+ */
+
+'use strict';
+
+require('dotenv').config()
+const debug = require('../src/lib/debug')
+
+const config = {
+  user: process.env.DOT4_USER
+  , password: process.env.DOT4_PASSWORD
+  , tenant: process.env.DOT4_TENANT
+  , baseUrl: process.env.DOT4_BASE_URL
+  , reloginTimeout: process.env.DOT4_RELOGIN_TIMEOUT || 1000 * 60 * 60 * 1 // 1h
+  , saKpiRepository: {
+	  url: process.env.sakpirepositoryurl
+	  , apiKey: process.env.sakpirepositoryapiKey
+  }
+};
+
+debug(`user: ${config.user}, tenant: ${config.tenant}, baseUrl: ${config.baseUrl}`)
+
+module.exports = config;
